8ce38bbda6442e0ecae12508c2a267fa4b71aa1d
Marvin Kweyu <mkweyu1@gmail.com>
2023-01-17 23:28:21 +0300

    chore: init checks for file upload

---
+++ soilcarbon/helpers/model_helpers.py
@@ -0,0 +1,3 @@
+def file_is_valid(csv_file):
+    """Check that the file is a CSV and with the relevant headers"""
+    pass
--- soilcarbon/models.py
+++ soilcarbon/models.py
@@ -1,4 +1,32 @@
 from django.db import models
+from django.utils.text import slugify
+from soilcarbon.helpers.model_helpers import file_is_valid
+
+
+class SourceFile(models.Model):
+    """CSV collection info from where field collection happens"""
+
+    title = models.CharField(max_length=500)
+    csv_file = models.FileField(upload_to="field_sources/%Y/%m/%d/")
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["title"]
+
+    def save(self, *args, **kwargs):
+        """Use the csv_file name as the title
+        # ToDo: Check that this is a valid CSV file format before saving this file
+        """
+        if file_is_valid(self.csv_file):
+            if not self.title:
+                self.title = slugify(self.title)
+                super().save(*args, **kwargs)
+            # Todo: add farms or create farm objects that have not been added from previous file uploads
+            # background task
+            #    create_farms_from_file(self.title)
+
+    def __str__(self):
+        return self.title
 
 
 class Farm(models.Model):
@@ -7,12 +35,17 @@ class Farm(models.Model):
     """
 
     farm_name = models.CharField(max_length=200, unique=True)
+    source_file = models.ForeignKey(
+        SourceFile, related_name="farms", on_delete=models.CASCADE
+    )
     location_boundary = models.CharField()
     soil_organic_carbon = models.PositiveIntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = "soil_organic_carbon"
+        ordering = ("soil_organic_carbon",)
+        # verbose_name = "farm"
+        # verbose_name_plural = "farms"
 
     def __str__(self) -> str:
         """Human readable name for this farm object"""
